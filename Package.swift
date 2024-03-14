be56bb69bf729c5acfec6634197a836f87168522
Aurelien Bidon <abidon@protonmail.com>
2024-03-14 17:00:18 -0500

    Bump crc-swift to 0.1.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/QuickBirdEng/crc-swift.git",
-            from: "0.1.0"
+            from: "0.1.1"
         ),
     ],
     targets: [
